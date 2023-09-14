6d45d1e4d9f12e0affa8f68959fc965f565b8229
michaelrafallo@gmail.com <michaelrafallo@gmail.com>
2023-09-15 04:15:30 +0800

    Updated data display and settings

--- helpers.php
+++ helpers.php
@@ -8,3 +8,25 @@ function base_url($param ='') {
 		$param
 	);
 }
+
+function get_items($key, $items, $list = false) {  
+
+    if( !is_array($items) ) {
+      return call_user_func_array($key, [$items]);
+    }
+
+    if( $list ) {
+      $data = '';
+      foreach( $items as $row ) {
+        $data .= '<li>'.call_user_func_array($key, [$row]).'</li>';
+      }      
+      return $data;
+    }
+
+    foreach( $items as $row ) {
+      $data[] = call_user_func_array($key, [$row]);
+    }
+
+    return implode(', ', $data);    
+
+}
