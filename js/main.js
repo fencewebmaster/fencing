9397f465c09bbb1c16011fff2be44dcff973dd67
darwinwaqaddi <darwin.waqaddi@egplusww.com>
2023-10-08 15:52:51 +0800

    Added global variables

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+//Global Variable
+var FENCES = FENCES || {};
+
+FENCES.el = {
+    'tabContainer': '.js-fencing-tab-container',
+    'tabArea': '.js-fencing-tab-container-area',
+    'fencingTab': '.fencing-tab'
+};
