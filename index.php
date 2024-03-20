af523a009982acf465ca24c5d5e015f56e0df50d
michaelrafallo@gmail.com <michaelrafallo@gmail.com>
2024-03-21 00:01:20 +0800

    Updated index

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ include 'helpers.php';
 
 $redirect_to = base_url('planner');
 
+/*
 $query_vars = $_SERVER['QUERY_STRING'] ? '?'.$_SERVER['QUERY_STRING'] : '';
 
 $queries = ['sid', 'site', 'qid'];
@@ -21,5 +22,6 @@ if( $not_found ) {
 	header("Location: ".$redirect_to.'?sid=1');
 	exit;
 }
+*/
 
 header("Location: ".$redirect_to.$query_vars);
