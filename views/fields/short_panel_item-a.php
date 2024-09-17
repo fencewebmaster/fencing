13824ab8cc6389833ecf7b65de00993c695bf917
michaelrafallo@gmail.com <michaelrafallo@gmail.com>
2024-09-18 04:39:24 +0800

    Updated glass pool short item

--- views/fields/short_panel_item-a.php
+++ views/fields/short_panel_item-a.php
@@ -7,16 +7,29 @@
 	data-cart-value="{{panel_value}}">
 
 	<div class="fencing-panel-item-size">{{panel_size}} {{panel_unit}}</div>
-	
-	<div class="fencing-panel-spigots">
-		<span class="fencing-panel-spigot fencing-btn-modal" data-key="post_options" data-target="#fc-control-modal"></span>
-		<span class="fencing-panel-spigot fencing-btn-modal" data-key="post_options" data-target="#fc-control-modal"></span>
+
+	<div class="fc-center-point fc-first-c-p">
+		<span class="fc-div-c-p"></span>
+	    <span class="fc-start-c-p">{{center_post}}</span>
+	    {{panel_size_center}}<br>
+	    Centers
 	</div>
 
-	<div class="fencing-panel-pinfixes">
+	<div class="fc-center-point fc-last-c-p">
+	    <span class="fc-div-c-p"></span>
+	    <span class="fc-div-c-p"></span>
+	    <span class="fc-end-c-p">{{center_post}}</span>
+	</div>
+
+    <div class="fencing-panel-spigots panel-post">
+		<span class="fencing-panel-spigot fencing-btn-modal" data-key="post_options" data-target="#fc-control-modal"></span>
+		<span class="fencing-panel-spigot fencing-btn-modal" data-key="post_options" data-target="#fc-control-modal"></span>		
+	</div>
+
+	<div class="fencing-panel-pinfixes" style="display: none;">
 		<span class="fencing-panel-pinfix"></span>
 		<span class="fencing-panel-pinfix"></span>
 		<span class="fencing-panel-pinfix"></span>						
 	</div>
-
+	    
 </div>	
